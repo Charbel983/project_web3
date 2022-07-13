e77483e5b24d790084e1fc5dad8476c18c5874a9
WTF <charbelsaba983@gmail.com>
2022-07-13 12:37:30 +0300

    test

--- api.js
+++ api.js
@@ -7,7 +7,6 @@ var dboperations = require('./dboperations');
 const Product = require('./product');
 const User = require('./user');
 
-app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
